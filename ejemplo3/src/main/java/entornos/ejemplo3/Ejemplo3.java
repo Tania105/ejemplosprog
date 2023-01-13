a532c51fe748f4bc9b54c53c9f7f0a96ecdba099
Tania105 <tchocanalexandre@iesmiguelherrero.com>
2023-01-13 12:16:14 +0100

    ejemplo de hola mundo

--- ejemplo3/src/main/java/entornos/ejemplo3/Ejemplo3.java
+++ ejemplo3/src/main/java/entornos/ejemplo3/Ejemplo3.java
@@ -12,6 +12,6 @@ package entornos.ejemplo3;
 public class Ejemplo3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Tania");
     }
 }
